0f98ac68606a565fc9eb6473ef115d2c17ab465f
johnsona9 <adamjohnson35@gmail.com>
2016-07-16 16:31:03 -0400

    basic game model, controller, and new game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
 class GamesController < ApplicationController
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game
+    else
+      flash[:danger] = 'player names not valid, please try again'
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:player_one, :player_two)
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  attr_accessor :player_one_15, :player_two_15, :player_one_16, :player_two_16, :player_one_17, :player_two_17, :player_one_18, :player_two_18, :player_one_19, :player_two_19, :player_one_20, :player_two_20, :player_one_be, :player_two_be
+  validates :player_one, :player_two, presence: true, length: { maximum: 20 }
 end
