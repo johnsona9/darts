7b76d8ecb607f132477c566fe618728e85d3a8da
johnsona9 <adamjohnson35@gmail.com>
2016-07-16 15:31:48 -0400

    base files for game model

---
+++ db/migrate/20160716173713_create_games.rb
@@ -0,0 +1,24 @@
+class CreateGames < ActiveRecord::Migration[5.0]
+  def change
+    create_table :games do |t|
+      t.string :player_one
+      t.string :player_two
+      t.integer :player_one_15
+      t.integer :player_two_15
+      t.integer :player_one_16
+      t.integer :player_two_16
+      t.integer :player_one_17
+      t.integer :player_two_17
+      t.integer :player_one_18
+      t.integer :player_two_18
+      t.integer :player_one_19
+      t.integer :player_two_19
+      t.integer :player_one_20
+      t.integer :player_two_20
+      t.integer :player_one_be
+      t.integer :player_two_be
+
+      t.timestamps
+    end
+  end
+end
