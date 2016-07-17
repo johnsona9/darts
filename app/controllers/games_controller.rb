f65f5bf90021bf1d146426d5ce8f4b7868380c06
johnsona9 <adamjohnson35@gmail.com>
2016-07-17 13:00:13 -0400

    initial game view complete
    needs major refactoring, buttons look horrible.
    each button press reloads the entire page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 class GamesController < ApplicationController
-
   def new
     @game = Game.new
   end
@@ -18,7 +17,134 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def player_one_15
+    @game = Game.find(params[:id])
+    @game.player_one_15 += 1
+    if @game.player_one_15 > 3 && @game.player_two_15 < 3
+      @game.player_one_score += 15
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_15
+    @game = Game.find(params[:id])
+    @game.player_two_15 += 1
+    if @game.player_two_15 > 3 && @game.player_one_15 < 3
+      @game.player_two_score += 15
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_one_16
+    @game = Game.find(params[:id])
+    @game.player_one_16 += 1
+    if @game.player_one_16 > 3 && @game.player_two_16 < 3
+      @game.player_one_score += 16
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_16
+    @game = Game.find(params[:id])
+    @game.player_two_16 += 1
+    if @game.player_two_16 > 3 && @game.player_one_16 < 3
+      @game.player_two_score += 16
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_one_17
+    @game = Game.find(params[:id])
+    @game.player_one_17 += 1
+    if @game.player_one_17 > 3 && @game.player_two_17 < 3
+      @game.player_one_score += 17
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_17
+    @game = Game.find(params[:id])
+    @game.player_two_17 += 1
+    if @game.player_two_17 > 3 && @game.player_one_17 < 3
+      @game.player_two_score += 17
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_one_18
+    @game = Game.find(params[:id])
+    @game.player_one_18 += 1
+    if @game.player_one_18 > 3 && @game.player_two_18 < 3
+      @game.player_one_score += 18
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_18
+    @game = Game.find(params[:id])
+    @game.player_two_18 += 1
+    if @game.player_two_18 > 3 && @game.player_one_18 < 3
+      @game.player_two_score += 18
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_one_19
+    @game = Game.find(params[:id])
+    @game.player_one_19 += 1
+    if @game.player_one_19 > 3 && @game.player_two_19 < 3
+      @game.player_one_score += 19
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_19
+    @game = Game.find(params[:id])
+    @game.player_two_19 += 1
+    if @game.player_two_19 > 3 && @game.player_one_19 < 3
+      @game.player_two_score += 19
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_one_20
+    @game = Game.find(params[:id])
+    @game.player_one_20 += 1
+    if @game.player_one_20 > 3 && @game.player_two_20 < 3
+      @game.player_one_score += 20
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_20
+    @game = Game.find(params[:id])
+    @game.player_two_20 += 1
+    if @game.player_two_20 > 3 && @game.player_one_20 < 3
+      @game.player_two_score += 20
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_one_be
+    @game = Game.find(params[:id])
+    @game.player_one_be += 1
+    if @game.player_one_be > 3 && @game.player_two_be < 3
+      @game.player_one_score += 25
+    end
+    redirect_to @game if @game.save
+  end
+
+  def player_two_be
+    @game = Game.find(params[:id])
+    @game.player_two_be += 1
+    if @game.player_two_be > 3 && @game.player_one_be < 3
+      @game.player_two_score += 25
+    end
+    redirect_to @game if @game.save
+  end
+
   private
+
     def game_params
       params.require(:game).permit(:player_one, :player_two)
     end
