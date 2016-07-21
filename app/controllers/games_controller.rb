a23e788a7d9b45f5ca180649b33cc1b398198390
johnsona9 <adamjohnson35@gmail.com>
2016-07-20 21:57:55 -0400

    reduction complete, testing time
    moved large number of controller methods to model and was able to shrink them

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  PLAYERS = ['one', 'two']
+
   def new
     @game = Game.new
   end
@@ -18,163 +20,22 @@ class GamesController < ApplicationController
   end
 
   def update_player
-    send('player_' + params[:player] + '_' + params[:score])
+    @game = Game.find(params[:id])
+    other_player = params[:player] == 'one' ? PLAYERS[1] : PLAYERS[0]
+    main_score = @game['player_' + params[:player] + '_' + params[:score]] || 0
+    other_score = @game['player_' + other_player + '_' + params[:score]] || 0
+    if main_score > 2 && other_score < 3
+      initial_score = @game[('player_' + params[:player] + '_score')]
+      initial_score ||= 0
+      @game[('player_' + params[:player] + '_score')] = initial_score + params[:score].to_i
+    end
+    if main_score < 3 || other_score < 3
+      initial_score = @game[('player_' + params[:player] + '_' + params[:score])]
+      initial_score ||= 0
+      @game[('player_' + params[:player] + '_' + params[:score])] = initial_score + 1
+    end
+    redirect_to @game if @game.save
   end
-
-  private
-    def player_one_15
-      @game = Game.find(params[:id])
-      if @game.player_one_15 < 3 || @game.player_two_15 < 3
-        @game.player_one_15 += 1
-      end
-      if @game.player_one_15 > 3 && @game.player_two_15 < 3
-        @game.player_one_score += 15
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_15
-      @game = Game.find(params[:id])
-      if @game.player_two_15 < 3 || @game.player_one_15 < 3
-        @game.player_two_15 += 1
-      end
-      if @game.player_two_15 > 3 && @game.player_one_15 < 3
-        @game.player_two_score += 15
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_one_16
-      @game = Game.find(params[:id])
-      if @game.player_one_16 < 3 || @game.player_two_16 < 3
-        @game.player_one_16 += 1
-      end
-      if @game.player_one_16 > 3 && @game.player_two_16 < 3
-        @game.player_one_score += 16
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_16
-      @game = Game.find(params[:id])
-      if @game.player_two_16 < 3 || @game.player_one_16 < 3
-        @game.player_two_16 += 1
-      end
-      if @game.player_two_16 > 3 && @game.player_one_16 < 3
-        @game.player_two_score += 16
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_one_17
-      @game = Game.find(params[:id])
-      if @game.player_one_17 < 3 || @game.player_two_17 < 3
-        @game.player_one_17 += 1
-      end
-      if @game.player_one_17 > 3 && @game.player_two_17 < 3
-        @game.player_one_score += 17
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_17
-      @game = Game.find(params[:id])
-      if @game.player_two_17< 3 || @game.player_one_17 < 3
-        @game.player_two_17 += 1
-      end
-      if @game.player_two_17 > 3 && @game.player_one_17 < 3
-        @game.player_two_score += 17
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_one_18
-      @game = Game.find(params[:id])
-      if @game.player_one_18 < 3 || @game.player_two_18 < 3
-        @game.player_one_18 += 1
-      end
-      if @game.player_one_18 > 3 && @game.player_two_18 < 3
-        @game.player_one_score += 18
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_18
-      @game = Game.find(params[:id])
-      if @game.player_two_18 < 3 || @game.player_one_18 < 3
-        @game.player_two_18 += 1
-      end
-      if @game.player_two_18 > 3 && @game.player_one_18 < 3
-        @game.player_two_score += 18
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_one_19
-      @game = Game.find(params[:id])
-      if @game.player_one_19 < 3 || @game.player_two_19 < 3
-        @game.player_one_19 += 1
-      end
-      if @game.player_one_19 > 3 && @game.player_two_19 < 3
-        @game.player_one_score += 19
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_19
-      @game = Game.find(params[:id])
-      if @game.player_two_19 < 3 || @game.player_one_19 < 3
-        @game.player_two_19 += 1
-      end
-      if @game.player_two_19 > 3 && @game.player_one_19 < 3
-        @game.player_two_score += 19
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_one_20
-      @game = Game.find(params[:id])
-      if @game.player_one_20 < 3 || @game.player_two_20 < 3
-        @game.player_one_20 += 1
-      end
-      if @game.player_one_20 > 3 && @game.player_two_20 < 3
-        @game.player_one_score += 20
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_20
-      @game = Game.find(params[:id])
-      if @game.player_two_20 < 3 || @game.player_one_20 < 3
-        @game.player_two_20 += 1
-      end
-      if @game.player_two_20 > 3 && @game.player_one_20 < 3
-        @game.player_two_score += 20
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_one_be
-      @game = Game.find(params[:id])
-      if @game.player_one_be < 3 || @game.player_two_be < 3
-        @game.player_one_be += 1
-      end
-      if @game.player_one_be > 3 && @game.player_two_be < 3
-        @game.player_one_score += 25
-      end
-      redirect_to @game if @game.save
-    end
-
-    def player_two_be
-      @game = Game.find(params[:id])
-      if @game.player_two_be < 3 || @game.player_one_be < 3
-        @game.player_two_be += 1
-      end
-      if @game.player_two_be > 3 && @game.player_one_be < 3
-        @game.player_two_score += 25
-      end
-      redirect_to @game if @game.save
-    end
 
     def game_params
       params.require(:game).permit(:player_one, :player_two)
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,8 @@
 class Game < ApplicationRecord
-  # attr_accessor :player_one_15, :player_two_15, :player_one_16, :player_two_16, :player_one_17, :player_two_17, :player_one_18, :player_two_18, :player_one_19, :player_two_19, :player_one_20, :player_two_20, :player_one_be, :player_two_be, :player_one_score, :player_two_score
+  attr_writer :player_one_15, :player_two_15, :player_one_16, :player_two_16, :player_one_17, :player_two_17, :player_one_18, :player_two_18, :player_one_19, :player_two_19, :player_one_20, :player_two_20, :player_one_be, :player_two_be, :player_one_score, :player_two_score
   validates :player_one, :player_two, presence: true, length: { maximum: 20 }
   validate :check_player_one_and_player_two
   before_save :init
-
-  def Game.HASHER
-    return [[@player_one_15, @player_two_15], [@player_one_16, @player_two_16], [@player_one_17, @player_two_17], [@player_one_18, @player_two_18], [@player_one_19, @player_two_19], [@player_one_20, @player_two_20], [@player_one_be, @player_two_be]]
-  end
 
   def init
     self.player_one_15 ||= 0
@@ -34,7 +30,72 @@ class Game < ApplicationRecord
     self.player_two_score ||= 0
   end
 
+  def player_one_15
+    @player_one_15 || 0
+  end
+
+  def player_two_15
+    @player_two_15 || 0
+  end
+
+  def player_one_16
+    @player_one_16 || 0
+  end
+
+  def player_two_16
+    @player_two_16 || 0
+  end
+
+  def player_one_17
+    @player_one_17 || 0
+  end
+
+  def player_two_17
+    @player_two_17 || 0
+  end
+
+  def player_one_18
+    @player_one_18 || 0
+  end
+
+  def player_two_18
+    @player_two_18 || 0
+  end
+
+  def player_one_19
+    @player_one_19 || 0
+  end
+
+  def player_two_19
+    @player_two_19 || 0
+  end
+
+  def player_one_20
+    @player_one_20 || 0
+  end
+
+  def player_two_20
+    @player_two_20 || 0
+  end
+
+  def player_one_be
+    @player_one_be || 0
+  end
+
+  def player_two_be
+    @player_two_be || 0
+  end
+
+  def player_one_score
+    @player_one_score || 0
+  end
+
+  def player_two_score
+    @player_two_score || 0
+  end
+
+
   def check_player_one_and_player_two
-    errors.add(:player_one, 'You cannot play against your@') if player_one == player_two
+    errors.add(:player_one, 'You cannot play against yourself') if player_one == player_two
   end
 end
