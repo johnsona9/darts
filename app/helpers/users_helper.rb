42207682b7066b38a7b2b3cfc7b9eb9446406660
johnsona9 <adamjohnson35@gmail.com>
2016-07-24 15:44:36 -0400

    user gravatar and listing each game played

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def gravatar_url(email, size)
+    gravatar = Digest::MD5::hexdigest(email).downcase
+    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+  end
+
+  def games_played
+    user = User.find(params[:id])
+    games_one = Game.where(player_one: user.id)
+    games_two = Game.where(player_two: user.id)
+    games_player = games_one + games_two
+  end
 end
