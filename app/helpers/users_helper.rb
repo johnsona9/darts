845d361137e54d350e2822b2206ac693ea9f8940
johnsona9 <adamjohnson35@gmail.com>
2016-07-25 18:55:17 -0400

    player page shows games won and lost

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,6 +8,22 @@ module UsersHelper
     user = User.find(params[:id])
     games_one = Game.where(player_one: user.id)
     games_two = Game.where(player_two: user.id)
-    games_player = games_one + games_two
+    @games_played = games_one + games_two
+  end
+
+  def winner(game)
+    player_one_score = game[:player_one_score] || 0
+    player_two_score = game[:player_two_score] || 0
+    if player_one_score > player_two_score && params[:id].to_i == game[:player_one]
+      return @winner = 'Win'
+    elsif player_one_score < player_two_score && params[:id].to_i == game[:player_one]
+      return @winner = 'Loss'
+    elsif player_one_score > player_two_score && params[:id].to_i == game[:player_two]
+      return @winner = 'Loss'
+    elsif player_one_score < player_two_score && params[:id].to_i == game[:player_two]
+      return @winner = 'Win'
+    else
+      return ''
+    end
   end
 end
