393d4758e8ea1fe748a24b1d278467375fa6d7ec
johnsona9 <adamjohnson35@gmail.com>
2016-07-30 14:14:10 -0400

    removing changing users to ints in game
    doing it from first migration

--- db/migrate/20160723233729_change_users_to_integers.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUsersToIntegers < ActiveRecord::Migration[5.0]
-  def change
-    change_column :games, :player_one, :integer
-    change_column :games, :player_two, :integer
-  end
-end
